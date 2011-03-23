d3bf9c4d53f9e3f6972067730bfe4bbc3e5c40ea
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-22 23:22:19 -0400

    Deleted scaffold. With Patricks help, wrote the whole thing by hand.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,83 +1,43 @@
 class RecipesController < ApplicationController
-  # GET /recipes
-  # GET /recipes.xml
   def index
     @recipes = Recipe.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @recipes }
-    end
   end
-
-  # GET /recipes/1
-  # GET /recipes/1.xml
+  
   def show
     @recipe = Recipe.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @recipe }
-    end
   end
-
-  # GET /recipes/new
-  # GET /recipes/new.xml
+  
   def new
     @recipe = Recipe.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @recipe }
-    end
   end
 
-  # GET /recipes/1/edit
+  def create
+    @recipe = Recipe.create(params[:recipe])
+    @recipe.published_at = DateTime.now
+    
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :action => :new
+    end
+  end
+  
   def edit
     @recipe = Recipe.find(params[:id])
   end
-
-  # POST /recipes
-  # POST /recipes.xml
-  def create
-    @recipe = Recipe.new(params[:recipe])
-
-    respond_to do |format|
-      if @recipe.save
-        format.html { redirect_to(@recipe, :notice => 'Recipe was successfully created.') }
-        format.xml  { render :xml => @recipe, :status => :created, :location => @recipe }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @recipe.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /recipes/1
-  # PUT /recipes/1.xml
+  
   def update
     @recipe = Recipe.find(params[:id])
-
-    respond_to do |format|
-      if @recipe.update_attributes(params[:recipe])
-        format.html { redirect_to(@recipe, :notice => 'Recipe was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @recipe.errors, :status => :unprocessable_entity }
-      end
+    if @recipe.update_attributes(params[:recipe])
+      redirect_to recipes_path
+    else
+      render :action => :new
     end
   end
-
-  # DELETE /recipes/1
-  # DELETE /recipes/1.xml
+  
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(recipes_url) }
-      format.xml  { head :ok }
-    end
   end
+  
 end
