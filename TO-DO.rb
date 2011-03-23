28096e196793fe33689495d9a1f8ad7a0494f7f8
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-23 09:40:48 -0400

    Created a to-do list of things to do. Self-descriptive enough?

---
+++ TO-DO.rb
@@ -0,0 +1,5 @@
+1. 'Rewrite the whole users_controller without looking. Delete it.'
+
+2. 'Make the getting started page work. Do I make a users controller action for it?'
+
+3. 'Get the Sessions stuff working. This will be a challenge.'
