7ee4da449331988fc9d4649db5accbe0200ae66d
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-24 17:42:13 -0400

    Updated seeds so that they actually work.

--- TO-DO.rb
+++ TO-DO.rb
@@ -1,9 +1,9 @@
-'COMMENTS'
+
+
+1. 'COMMENTS'
 
 2. 'Get the Sessions stuff working. This will be a challenge. CONNECT RECIPES TO USERS'
 
 3. 'Delete confirmation coming up when user not logged in'
-
-4. 'One can continually log in, albeit its a different session'
 
 5. 'What to do about the pages in PUBLIC? Esp. regarding the Log in | Log out aspect.'
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 user = User.create :email => 'mary@example.com',
                    :password => 'secret',
                    :password_confirmation => 'secret'
+user.save
 Cuisine.create [{:name => 'French'},
                  {:name => 'Italian'},
                  {:name => 'Mexican'},
@@ -17,15 +18,20 @@ Course.create [{:name => 'Appetizer'},
                 {:name => 'Entree'},
                 {:name => 'Desert'}]
             
-user.recipes.create :title => 'Test Recipe',
-                :ingredients => '1 of this, 2 of these, 1 lb of this, 1 oz of that',
-                :directions => 'Bake at 350 degrees for 1 hour',
-                :published_at => Date.today
-user.recipes.create :title => 'Second Test Recipe',
+Recipe.create :title => 'Bobby\'s Hominy',
+              :excerpt => 'Oh this recipe will...',
+              :ingredients => '1 of this, 2 of these, 1 lb of this, 1 oz of that',
+              :directions => 'Bake at 350 degrees for 1 hour',
+              :published_at => Date.today
+
+recipe2 = Recipe.create :title => 'MaaMaa\'s Finest',
+                :excerpt => 'My grandmother.....',
                 :ingredients => '5oz of this, 2 lbs of this, 1 lb of this, 12 oz of that',
                 :directions => 'Bake at 425 degrees for 1 hour',
                 :published_at => Date.today
-user.recipes.create :title => 'Third Test Recipe',
+
+recipe3 = Recipe.create :title => 'Breakfast Casserole',
+                :excerpt => 'Have you ever tasted...',
                 :ingredients => '12oz of this, 1 lb of this, 9 lb of this, 25 oz of that',
                 :directions => 'Bake at 425 degrees for 1 hour',
                 :published_at => Date.today
