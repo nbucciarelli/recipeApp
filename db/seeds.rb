3dfc32596fb60e515ee1d949f3663904c033348b
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-24 21:54:02 -0400

    Removed excerpt from seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,19 +19,16 @@ Course.create [{:name => 'Appetizer'},
                 {:name => 'Desert'}]
             
 Recipe.create :title => 'Bobby\'s Hominy',
-              :excerpt => 'Oh this recipe will...',
               :ingredients => '1 of this, 2 of these, 1 lb of this, 1 oz of that',
               :directions => 'Bake at 350 degrees for 1 hour',
               :published_at => Date.today
 
 recipe2 = Recipe.create :title => 'MaaMaa\'s Finest',
-                :excerpt => 'My grandmother.....',
                 :ingredients => '5oz of this, 2 lbs of this, 1 lb of this, 12 oz of that',
                 :directions => 'Bake at 425 degrees for 1 hour',
                 :published_at => Date.today
 
 recipe3 = Recipe.create :title => 'Breakfast Casserole',
-                :excerpt => 'Have you ever tasted ...',
                 :ingredients => '12oz of this, 1 lb of this, 9 lb of this, 25 oz of that',
                 :directions => 'Bake at 425 degrees for 1 hour',
                 :published_at => Date.today
