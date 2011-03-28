88d5370df50b9ebd3738c81cf6d0dd0afa1c44a2
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-28 16:54:10 -0400

    added another user (admin) and some more default recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+User.create :username => 'admin',
+                   :email => 'admin@example.com',
+                   :password => 'secret',
+                   :password_confirmation => 'secret'
+                   
 User.create :username => 'Mary',
                    :email => 'mary@example.com',
                    :password => 'secret',
@@ -37,3 +42,13 @@ Recipe.create :title => 'Breakfast Casserole',
                 :ingredients => '12oz of this, 1 lb of this, 9 lb of this, 25 oz of that',
                 :directions => 'Bake at 425 degrees for 1 hour',
                 :published_at => Date.today
+                
+Recipe.create :title => 'World\'s Best Salsa',
+              :ingredients => '1 of this, 2 of these, 1 lb of this, 1 oz of that',
+              :directions => 'Bake at 350 degrees for 1 hour',
+              :published_at => Date.today
+              
+Recipe.create :title => 'Beer Baked Chicken',
+              :ingredients => '1 of this, 2 of these, 1 lb of this, 1 oz of that',
+              :directions => 'Bake at 350 degrees for 1 hour',
+              :published_at => Date.today
