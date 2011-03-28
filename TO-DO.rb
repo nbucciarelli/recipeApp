884209206b7266570dfc489a95284673c12f023f
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-28 16:54:28 -0400

    Updated to-do list

--- TO-DO.rb
+++ TO-DO.rb
@@ -1,10 +1,10 @@
 
-0. 'Add cuisine.rb actions (a controller. In future, for admins)'
-
 1. 'COMMENTS'
 
-2. 'Get the Sessions stuff working. This will be a challenge. CONNECT RECIPES TO USERS'
+*2. 'Get the Sessions stuff working. This will be a challenge. CONNECT RECIPES TO USERS'
 
 3. 'Delete confirmation coming up when user not logged in'
 
 5. 'What to do about the pages in PUBLIC? Esp. regarding the Log in | Log out aspect.'
+
+6. 'Make admin links'
