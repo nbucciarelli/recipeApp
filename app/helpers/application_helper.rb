29a1745739155f508d0c4607b0f4e74194609434
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-28 16:53:00 -0400

    Tried to do admin_links. Instead inserted a comment reminder.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,28 @@ module ApplicationHelper
   def log_in_out_links(current_user)
     if current_user.nil?
       link = content_tag(:li) do
-        link_to('Log in', login_path)
+        link_to 'Log in', login_path
       end
     else
       link = "Welcome, #{current_user[:username]}!" +
       content_tag(:br) +
       content_tag(:li) do
-        link_to('Log out', logout_path)
+        link_to 'Log out', logout_path
       end
     end
     link.html_safe
   end
+  
+  # def admin_links(current_user)
+  #   if current_user.username == 'admin'
+  #     link = content_tag(:li) +
+  #     content_tag(:h3, "Admin Links") +
+  #     content_tag(:ul, :class => 'links') +
+  #     content_tag(:li) do
+  #       link_to 'Types of Cuisine', cuisine_path
+  #     end
+  #   else
+  #     
+  #   end
+  # end
 end
