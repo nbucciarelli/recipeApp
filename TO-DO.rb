f41cbdcee880013245d5cbfe99ddcfe0897fddf4
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-29 16:41:31 -0400

    updated todo list

--- TO-DO.rb
+++ TO-DO.rb
@@ -1,10 +1,9 @@
 
-1. 'COMMENTS'
-
 *2. 'Get the Sessions stuff working. This will be a challenge. CONNECT RECIPES TO USERS'
 
-3. 'Delete confirmation coming up when user not logged in'
 
-5. 'What to do about the pages in PUBLIC? Esp. regarding the Log in | Log out aspect.'
+1. 'COMMENTS'
+
+3. 'Delete confirmation coming up when user not logged in'
 
 6. 'Make admin links'
