2c7b5fa08eb1b41af8a4a0d49fa221a0c4c18c47
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-29 08:55:36 -0400

    Deleted and recreated cuisine files for practice; then added to them.

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,5 +1,6 @@
 class CuisinesController < ApplicationController
-  
+  before_filter :authenticate
+
   def index
     @cuisines = Cuisine.all
   end
@@ -8,12 +9,36 @@ class CuisinesController < ApplicationController
     @cuisine = Cuisine.find(params[:id])
   end
   
-  def edit
+  def new
+    @cuisine = Cuisine.new
+  end
+  
+  def create
+    @cuisine = Cuisine.create(params[:cuisine])
     
+    if @cuisine.save
+      redirect_to cuisines_path, :notice => 'Successfully created new cuisine'
+    else
+      render :action => :new
+    end
+  end
+  
+  def edit
+    @cuisine = Cuisine.find(params[:id])
+  end
+  
+  def update
+    @cuisine = Cuisine.find(params[:id])
+    if @recipe.update_attributes(params[:cuisine])
+      redirect_to cuisines_path, :notice => 'Successfully updated cuisine'
+    else
+      render :action => :new
+    end
   end
   
   def destroy
     @cuisine = Cuisine.find(params[:id])
     @cuisine.destroy
+    redirect_to cuisines_path, :notice => 'Successfully deleted cuisine'
   end
 end
--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -2,4 +2,6 @@ class Cuisine < ActiveRecord::Base
   has_many :categorizations
   has_many :recipes, :through => :categorizations
   has_many :courses, :through => :categorizations
+  
+  validates :name, :uniqueness => true
 end
