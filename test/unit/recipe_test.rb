d2e8a885ac3cf2f1e887bb40913eccd5f00b45d6
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-30 15:38:06 -0400

    First unit tests! CRUD

--- test/unit/recipe_test.rb
+++ test/unit/recipe_test.rb
@@ -2,14 +2,17 @@ require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
   
+  #-----------------------------
+  #-----Testing CRUD------------
+  #-----------------------------
   test 'should create recipe' do
     recipe = Recipe.new
     
     recipe.user = users(:eugene)
-    recipe.title = 'Best recipe everrrr'
-    recipe.ingredients = '1 of these and 3 of these'
-    recipe.directions = 'Do this an this and this!'
-    recipe.published_at = Date.today
+    recipe.title = 'Oh my the best thing ever'
+    recipe.ingredients = '1 of this 4 of that'
+    recipe.directions = 'You had better do this...'
+    recipe.published_at = DateTime.now
     
     assert recipe.save
   end
@@ -19,8 +22,37 @@ class RecipeTest < ActiveSupport::TestCase
     assert !recipe.save
   end
   
-  test 'should find recipe' do
+  test 'should find a recipe' do
     recipe_id = recipes(:best_recipe).id
-    assert_nothing_raised { Recipe.find(recipe_id) }
+    recipe = Recipe.find(recipe_id)
+    
+    assert_nothing_raised { recipe }
   end
+  
+  test 'should update recipe' do
+    recipe = recipes(:best_recipe)
+    assert recipe.update_attributes(:title => 'New Recipe Title')
+  end
+  
+  test 'should NOT update recipe' do
+    recipe = recipes(:best_recipe)
+    assert !recipe.update_attributes(:title => '')
+  end
+  
+  test 'should destroy recipe' do
+    recipe = recipes(:best_recipe)
+    recipe.destroy
+    assert_raise(ActiveRecord::RecordNotFound) { Recipe.find(recipe.id) }
+  end
+  
+  #----------Is this test useless?
+  test 'should NOT destroy recipe' do
+    recipe = recipes(:best_recipe)
+    assert_nothing_raised(ActiveRecord::RecordNotFound) { Recipe.find(recipe.id) }
+  end
+  
+  #-----------------------------
+  #---Testing Validations-------
+  #-----------------------------
+  
 end
