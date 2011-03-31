0a78cb2489e1f193cb8d399a54ca3e10adc913ff
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-31 11:44:21 -0400

    Added tests, but without any code. Soon to come

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,7 +2,31 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test 'should create a new user' do
+    
   end
+  
+  test 'should find a user' do
+    
+  end
+  
+  test 'should update a user' do
+    
+  end
+  
+  test 'should destroy a user' do
+    
+  end
+  
+  #------------------
+  #----Validations---
+  #------------------
+  test 'should NOT create a user without a name, email, password, or password_confirmation' do
+    
+  end
+  
+  test 'should NOT update a user without a name, email, password, or password_confirmation' do
+    
+  end
+
 end
