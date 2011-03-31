5cb59f325802906ce4cf81a83e1f4935ca96c147
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-31 10:34:40 -0400

    Created cuisine test and some tests

--- test/unit/cuisine_test.rb
+++ test/unit/cuisine_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class CuisineTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test 'should create a new cuisine' do
+    cuisine = Cuisine.new
+    cuisine.name = 'Mongolian'
+    
+    assert cuisine.save
   end
+  
+  test 'should NOT create a new cuisine' do
+    cuisine = Cuisine.new
+    assert !cuisine.save
+  end
+  
+  test 'should find a cuisine' do
+    cuisine_id = cuisines(:mexican).id
+    assert_nothing_raised { Cuisine.find(cuisine_id) }
+  end
+  
 end
