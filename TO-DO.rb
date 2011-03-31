9efd54e0e47f56d3afafebeb44b718f9a1733ee9
Robert Pearce <robertwaltonpearce@gmail.com>
2011-03-31 11:10:44 -0400

    updated to-do list

--- TO-DO.rb
+++ TO-DO.rb
@@ -1,16 +1,12 @@
 
-*-1. 'Write some tests!'
-
 *0. 'Where is that damn 7 coming from?'
 
 *1. 'Figure out how profiles should work'
-
-
-2. 'Get the Sessions stuff working. This will be a challenge. CONNECT RECIPES TO USERS'
-
 
 1. 'COMMENTS'
 
 3. 'Delete confirmation coming up when user not logged in'
 
 6. 'Make admin links'
+
+7. 'If you edit a cuisine/recipe/user, delete the title/name, and get a validation error, the name disappears.'
