6b59f0485078040a00f46a7a9df2dba4bebddf86
Robert Pearce <robertwaltonpearce@gmail.com>
2011-04-01 11:50:29 -0400

    tested on this to delete whitespace on save

--- TO-DO.rb
+++ TO-DO.rb
@@ -1,15 +1,13 @@
 1. 'COMMENTS'
 
-3. 'Delete confirmation coming up when user not logged in'
+(BUG)3. 'Delete confirmation coming up when user not logged in'
 
 6. 'Make admin links'
 
-7. 'If you edit a cuisine/recipe/user, delete the title/name, and get a validation error, the name disappears.'
-
 8. 'logout constantly present in background?'
 
-ideas
 
-'description -> trunc'
-'move nav bar stuff to above folder thing'
-'will_paginate'
+ideas
+- 'description -> trunc'
+- 'move nav bar stuff to above folder thing'
+- 'will_paginate'
