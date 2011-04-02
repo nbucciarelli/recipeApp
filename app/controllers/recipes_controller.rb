7ea47dc0ebbaf8e4d523b43bda6b18734e4c39e0
Robert Pearce <robertwaltonpearce@gmail.com>
2011-04-01 21:15:20 -0400

    added time_ago_in_words to recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,14 @@
 class RecipesController < ApplicationController
   before_filter :authenticate, :except => [:index, :show]
-  
+
   def index
     @recipes = Recipe.all
   end
-  
+
   def show
     @recipe = Recipe.find(params[:id])
   end
-  
+
   def new
     @recipe = Recipe.new
   end
@@ -16,22 +16,22 @@ class RecipesController < ApplicationController
   def create
     @recipe = current_user.recipes.new(params[:recipe])
     @recipe.published_at = DateTime.now
-    
+
     if @recipe.save
       redirect_to recipes_path, :notice => 'Successfully created new recipe'
     else
       render :action => :new
     end
   end
-  
+
   def edit
     @recipe = current_user.recipes.find(params[:id])
   end
-  
+
   def update
     params[:recipe][:cuisine_ids] ||= []
     params[:recipe][:course_ids] ||= []
-    
+
     @recipe = current_user.recipes.find(params[:id])
     if @recipe.update_attributes(params[:recipe])
       redirect_to recipes_path, :notice => 'Successfully updated recipe'
@@ -39,15 +39,15 @@ class RecipesController < ApplicationController
       render :action => :edit
     end
   end
-  
+
   def destroy
     @recipe = current_user.recipes.find(params[:id])
     @recipe.destroy
     redirect_to root_path, :notice => 'Successfully deleted recipe'
   end
-  
+
   def my_recipes
     # @recipes = current_user.recipes.all
   end
-  
+
 end
