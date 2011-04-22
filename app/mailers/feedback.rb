a4052250a7436776616e7673789487cafb162ef4
Robert Pearce <robertwaltonpearce@gmail.com>
2011-04-22 15:08:49 -0400

    trying to fix feedback errors..

--- app/mailers/feedback.rb
+++ app/mailers/feedback.rb
@@ -1,13 +1,13 @@
-class Feedback < ActionMailer::Base
-  default :from => "example@example.com"
-  
-  def feedback(email_params, sent_at = Time.now)
-    # subject "Electronic Mail: " << 'Feedback for RecipeApp!'
-    # recipients "robertwaltonpearce@gmail.com"
-    # # name email_params[:name]
-    # ease_of_use email_params[:ease_of_use]
-    # from email_params[:address]
-    # sent_on sent_at
-    # body
-  end
-end
+# class Feedback < ActionMailer::Base
+#   # default :from => "example@example.com"
+#
+#   def feedback(email_params, sent_at = Time.now)
+#     # subject "Electronic Mail: " << 'Feedback for RecipeApp!'
+#     # recipients "robertwaltonpearce@gmail.com"
+#     # # name email_params[:name]
+#     # ease_of_use email_params[:ease_of_use]
+#     # from email_params[:address]
+#     # sent_on sent_at
+#     # body
+#   end
+# end
