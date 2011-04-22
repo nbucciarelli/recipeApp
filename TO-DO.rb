708d88842055d88bd6052b6203b8d8fc47125a57
Robert Pearce <robertwaltonpearce@gmail.com>
2011-04-21 23:41:52 -0400

    Messed with box shadows. Added link_tos on getting started

--- TO-DO.rb
+++ TO-DO.rb
@@ -1,8 +1,8 @@
 1. 'COMMENTS'
---------'http://localhost:3000/recipes/1' => "get users's username""
+--------'http://localhost:3000/recipes/1' => "get users's username"
 
 (BUG)3. 'Delete confirmation coming up when user not logged in'
-
+(BUG)4. 'admin can only edit his profile--need to set up roles'
 
 ideas
 - 'description -> trunc'
