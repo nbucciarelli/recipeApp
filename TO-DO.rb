6505278e4a91549cac6b984819dd73e4d7b18583
Robert Pearce <robertwaltonpearce@gmail.com>
2011-04-26 08:53:57 -0400

    Updated to-do list

--- TO-DO.rb
+++ TO-DO.rb
@@ -1,6 +1,7 @@
-1. 'COMMENTS'
---------'http://localhost:3000/recipes/1' => "get users's username"
 
+--------'http://localhost:3000/recipes/1' => "get users's username"
+0. 'Make feedback form work'
+1. 'Make it so users can upload pictures of food'
 (BUG)3. 'Delete confirmation coming up when user not logged in'
 (BUG)4. 'admin can only edit his profile--need to set up roles'
 
