8937a20d2e2c710937fa8481b84c4edb413f6434
Robert Pearce <robertwaltonpearce@gmail.com>
2011-04-26 08:57:15 -0400

    Added recipe image helper

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,10 @@
 module RecipesHelper
+
+  def display_recipe_image(recipe)
+    if recipe.recipe_image_file_name.present?
+      image_tag("../../images/#{recipe.recipe_image_file_name}")
+    else
+      image_tag("../../images/tikihut.jpg")
+    end
+  end
 end
