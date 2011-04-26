36e43b43c54791e6f4311ee6dd68d50d69ab32e2
Robert Pearce <robertwaltonpearce@gmail.com>
2011-04-26 08:54:35 -0400

    Updated redirects.

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -4,41 +4,40 @@ class CuisinesController < ApplicationController
   def index
     @cuisines = Cuisine.all
   end
-  
+
   def show
     @cuisine = Cuisine.find(params[:id])
   end
-  
+
   def new
     @cuisine = Cuisine.new
   end
-  
+
   def create
     @cuisine = Cuisine.create(params[:cuisine])
-    
     if @cuisine.save
-      redirect_to cuisines_path, :notice => 'Successfully created new cuisine'
+      redirect_to cuisines_path, :notice => 'Successfully created new cuisine.'
     else
-      render :action => :new
+      render :new
     end
   end
-  
+
   def edit
     @cuisine = Cuisine.find(params[:id])
   end
-  
+
   def update
     @cuisine = Cuisine.find(params[:id])
     if @cuisine.update_attributes(params[:cuisine])
-      redirect_to cuisines_path, :notice => 'Successfully updated cuisine'
+      redirect_to cuisines_path, :notice => 'Successfully updated cuisine.'
     else
-      render :action => :edit
+      render :edit
     end
   end
-  
+
   def destroy
     @cuisine = Cuisine.find(params[:id])
     @cuisine.destroy
-    redirect_to cuisines_path, :notice => 'Successfully deleted cuisine'
+    redirect_to cuisines_path, :notice => 'Successfully deleted cuisine.'
   end
 end
