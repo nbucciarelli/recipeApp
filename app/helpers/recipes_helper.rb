e6b994e339378d40b5258524f37028122ebb8235
Robert Pearce <robertwaltonpearce@gmail.com>
2011-04-26 13:22:45 -0400

    testing something

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -2,7 +2,7 @@ module RecipesHelper
 
   def display_recipe_image(recipe)
     if recipe.recipe_image_file_name.present?
-      image_tag("../../images/#{recipe.recipe_image_file_name}")
+      image_tag("../../images/food_small.jpg")
     else
       image_tag("../../images/tikihut_small.jpg")
     end
