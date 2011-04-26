01d256e26da3eba011fed2a566361d4de8287b18
Robert Pearce <robertwaltonpearce@gmail.com>
2011-04-26 08:57:55 -0400

    Just moved the validations to above the belongs_to

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
-  belongs_to :recipe
   validates :name, :email, :body, :presence => true
   validate :recipe_should_be_published
+
+  belongs_to :recipe
 
   def recipe_should_be_published
     errors.add(:recipe_id, "is not published yet") if recipe && !recipe.published?
