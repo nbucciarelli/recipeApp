131d6f04eff3147b21f0da4e1cd5683e3c8b6c18
Robert Pearce <robertwaltonpearce@gmail.com>
2011-04-27 08:38:57 -0400

    Added commented out scope

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -2,7 +2,9 @@ class Cuisine < ActiveRecord::Base
   has_many :categorizations
   has_many :recipes, :through => :categorizations
   has_many :courses, :through => :categorizations
-  
+
   validates :name, :uniqueness => true,
                    :presence => true
+
+  # scope :order_by_name, order('name')
 end
